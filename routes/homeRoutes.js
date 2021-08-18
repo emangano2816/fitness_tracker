e132f9a0d0153746ae24e1353257d4a5fc4d69d3
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-18 18:07:25 -0400

    Added routes to navigate the various html pages.

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
+const path = require("path");
 
-//GET fitness tracker link
-router.get("/", (req, res) => {
-    res.render("index");
-});
+//index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
 
-//GET Dashboard 
-router.get("/stats", (req, res) => {
-    res.render("stats")
-});
+//stats.html
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+})
+
+//exercise.html
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+})
 
 module.exports = router;
