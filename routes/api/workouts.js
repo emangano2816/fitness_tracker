9146b2fe3c73b947788a3582b2aaf51122852eff
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-19 23:02:15 -0400

    Added PUT api/workouts/:id: exercises can now be added to a workout.

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { Mongoose } = require("mongoose");
 const db = require('../../models');
 
 
@@ -25,21 +26,34 @@ router.get('/', (req, res) => {
 //GET api/workouts/range - return last 7 workouts with totalDuration computed
 router.get('/range', (req, res) => {
     db.Workout
-    .aggregate(
-                [
-                    {
-                        $addFields: { totalDuration: {$sum: "$exercises.duration"}}
-                    }
-                ]
-    )
-    .sort({ day: -1 })
-    .limit(7)
-    .then (dbWorkout => {
-            res.json(dbWorkout);
-    })
-    .catch(err => {
+        .aggregate(
+                    [
+                        {
+                            $addFields: { totalDuration: {$sum: "$exercises.duration"}}
+                        }
+                    ]
+        )
+        .sort({ day: -1 })
+        .limit(7)
+        .then (dbWorkout => {
+                res.json(dbWorkout);
+        })
+        .catch(err => {
             res.json(err);
-    })
+        })
+});
+
+//PUT api/workouts
+router.put('/:id', ({ body, params }, res) => {
+    db.Workout
+        .findByIdAndUpdate(params.id, {$push: {exercises: body}}, {new: true, runValidators: true})
+        .then (dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        })
 });
 
 module.exports = router;
