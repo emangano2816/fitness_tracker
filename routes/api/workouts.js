11e96b2f820d14cdc449f256d3e72c363c54f575
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-21 08:52:53 -0400

    Added comments and resonse status to routes.

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -3,7 +3,7 @@ const { Mongoose } = require("mongoose");
 const db = require('../../models');
 
 
-//GET api/workouts - return last workout with totalDuration computed
+//GET api/workouts - calculate totalDuration and then return last workout (sort descending, limit 1)
 router.get('/', (req, res) => {
     db
     .Workout
@@ -15,15 +15,17 @@ router.get('/', (req, res) => {
                 ])
     .sort({ day: -1})
     .limit(1)
+    //if successful, then return data
     .then(dbWorkout => {
-            res.json(dbWorkout);
+            res.status(200).json(dbWorkout);
     })
+    //otherwise, return error
     .catch(err => {
-            res.json(err);
+            res.status(400).json(err);
     })
 });
 
-//GET api/workouts/range - return last 7 workouts with totalDuration computed
+//GET api/workouts/range - calculate totalDuration and then return last 7 workouts (sort descending, limit 7)
 router.get('/range', (req, res) => {
     db.Workout
         .aggregate(
@@ -35,11 +37,13 @@ router.get('/range', (req, res) => {
         )
         .sort({ day: -1 })
         .limit(7)
+        //if successful, then return data
         .then (dbWorkout => {
-                res.json(dbWorkout);
+                res.status(200).json(dbWorkout);
         })
+        //otherwise, return error
         .catch(err => {
-            res.json(err);
+            res.status(400).json(err);
         })
 });
 
@@ -47,26 +51,30 @@ router.get('/range', (req, res) => {
 router.put('/:id', ({ body, params }, res) => {
     db.Workout
         .findByIdAndUpdate(params.id, {$push: {exercises: body}}, {new: true, runValidators: true})
+        //if successful, then return data
         .then (dbWorkout => {
-            res.json(dbWorkout);
+            res.status(200).json(dbWorkout);
         })
+        //otherwise, return error
         .catch(err => {
             console.log(err);
-            res.json(err);
+            res.status(400).json(err);
         })
 });
 
-//POST api/workouts
+//POST api/workouts - create new workout (i.e., add new workout to db)
 router.post('/', ({ body }, res) => {
     db.Workout
         .create(body)
+        //if successful, return data
         .then(dbWorkout => {
             console.log(dbWorkout);
-            res.json(dbWorkout);
+            res.status(200).json(dbWorkout);
         })
+        //otherwsie, return error
         .catch(err => {
             console.log(err);
-            res.json(err);
+            res.status(400).json(err);
         })
 })
 
